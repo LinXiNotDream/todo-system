e67c5645952d54bcca16f3a607923e130ce51aa5
Zoom <1113525779@qq.com>
2020-11-15 21:58:34 +0800

    初始化项目

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,9 @@ module.exports = {
     node: true
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint"
   ],
@@ -14,7 +14,11 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 0,
+    'no-unused-vars': 'warn',
+    semi: [0],
+    singleQuote: 0,
   }
-};
+}
